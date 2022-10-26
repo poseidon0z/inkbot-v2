33c6e1bbb3fe1c8812cb0e43e86b363293e97c75
poseidon0z <adityaprabhu1290@gmail.com>
2022-10-26 15:46:10 +0530

    Added the alive command

--- cogs/bot_utils.py
+++ cogs/bot_utils.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 
-class botUtil(commands.Cog):
+class BotUtil(commands.Cog):
     """This cog consists of util commands for ink
 
     Commands
@@ -21,7 +21,6 @@ class botUtil(commands.Cog):
         print(f"{__class__.__name__} ready")
 
     @commands.command(name="sync")
-    @commands.is_owner()
     async def sync(self, ctx) -> None:
         """Globally syncs all slash commands"""
         print("Commands syncing")
@@ -32,10 +31,16 @@ class botUtil(commands.Cog):
             print(e)
 
         channel = self.bot.get_channel(838425596421079060)
+        await ctx.send(f"{self.bot.user} ({self.bot.user.id}) has synced {len(synced)} commands successfully!")
         await channel.send(f"{self.bot.user} ({self.bot.user.id}) has synced {len(synced)} commands successfully!")
         print("Commands synced successfully")
 
+    @commands.command(name="alive")
+    async def alive(self, ctx) -> None:
+        """Say "I'm alive" in response to message sent
+        Used to check if bot is alive"""
+        await ctx.send("I'm alive")
 
 # running the setup for the cog
 async def setup(bot):
-    await bot.add_cog(botUtil(bot))
+    await bot.add_cog(BotUtil(bot))
